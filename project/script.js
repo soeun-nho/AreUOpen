091728d5d4da1fd56fe01c8d40595808af69e70d
ehgmlKim <dohee930@gmail.com>
2022-07-26 12:18:14 +0900

    페이지 넘어가는 기능 구현 7/26 12:18pm

--- project/script.js
+++ project/script.js
@@ -6,19 +6,20 @@ function start() {
 }
 
 var num = 1; //현재 몇번 문제에 있는지를 나타내는 변수
-var q = {
-   1: {"title": "1번 문제", "type": "UD", "A": "대답 U", "B": "대답 D"},
-   2: {"title": "2번 문제", "type": "UD", "A": "대답 U", "B": "대답 D"},
-   3: {"title": "3번 문제", "type": "UD", "A": "대답 U", "B": "대답 D"},
+var q = { //문제 및 답변 내용
+   1: {"title": "1번 문제", "type": "UD", "A": "대답 U1", "B": "대답 D1"},
+   2: {"title": "2번 문제", "type": "UD", "A": "대답 U2", "B": "대답 D2"},
+   3: {"title": "3번 문제", "type": "UD", "A": "대답 U3", "B": "대답 D3"},
    
-   4: {"title": "4번 문제", "type": "HS", "A": "대답 H", "B": "대답 S"},
-   5: {"title": "5번 문제", "type": "HS", "A": "대답 H", "B": "대답 S"},
-   6: {"title": "6번 문제", "type": "HS", "A": "대답 H", "B": "대답 S"},
+   4: {"title": "4번 문제", "type": "HS", "A": "대답 H1", "B": "대답 S1"},
+   5: {"title": "5번 문제", "type": "HS", "A": "대답 H2", "B": "대답 S2"},
+   6: {"title": "6번 문제", "type": "HS", "A": "대답 H3", "B": "대답 S3"},
 
-   7: {"title": "7번 문제", "type": "AT", "A": "대답 A", "B": "대답 T"},
-   8: {"title": "8번 문제", "type": "AT", "A": "대답 A", "B": "대답 T"},
-   9: {"title": "9번 문제", "type": "AT", "A": "대답 A", "B": "대답 T"}
+   7: {"title": "7번 문제", "type": "AT", "A": "대답 A1", "B": "대답 T1"},
+   8: {"title": "8번 문제", "type": "AT", "A": "대답 A2", "B": "대답 T2"},
+   9: {"title": "9번 문제", "type": "AT", "A": "대답 A3", "B": "대답 T3"}
 }
+//버튼 누르면 다음 문제 내용 불러오는 함수
 function next() { 
     $("#title").html(q[num]["title"]); 
     $("#type").val(q[num]["type"]); 
@@ -28,8 +29,8 @@ function next() {
  }
  //답안 버튼 눌렀을 때
  $("#A").click(function () {
-    //next();
-    document.write($("#title").val("어쩌구"));
+    next();
+    
 });
 $("#B").click(function () {
     next();
