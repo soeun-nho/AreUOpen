d5aaffb67e8e97c9e4b66acf47ca3dd6b73af7f1
ehgmlKim <dohee930@gmail.com>
2022-07-26 13:19:01 +0900

    결과창 연결하는 함수 대충 구현 7/26 1:18pm

--- project/script.js
+++ project/script.js
@@ -1,10 +1,3 @@
-//시작하기 누르면 시작페이지 사라지고 문제페이지 보임
-function start() {
-    $("#start").hide();
-    $("#question").show();
-    next();
-}
-
 var num = 1; //현재 몇번 문제에 있는지를 나타내는 변수
 var q = { //문제 및 답변 내용
    1: {"title": "1번 문제", "type": "UD", "A": "대답 U1", "B": "대답 D1"},
@@ -19,16 +12,38 @@ var q = { //문제 및 답변 내용
    8: {"title": "8번 문제", "type": "AT", "A": "대답 A2", "B": "대답 T2"},
    9: {"title": "9번 문제", "type": "AT", "A": "대답 A3", "B": "대답 T3"}
 }
+
+//시작하기 누르면 시작페이지 사라지고 문제페이지 보임
+function start() {
+    $("#start").hide();
+    $("#question").show();
+    next();
+}
+
 //버튼 누르면 다음 문제 내용 불러오는 함수
 function next() { 
-    $("#title").html(q[num]["title"]); 
-    $("#type").val(q[num]["type"]); 
-    $("#A").html(q[num]["A"]); 
-    $("#B").html(q[num]["B"]);
-    num++; 
+    if(num==10) {
+        var mbti = ""; 
+    
+      ($("#UD").val() < 2) ? mbti += "D" : mbti += "U";
+      ($("#HS").val() < 2) ? mbti += "S" : mbti += "H";
+      ($("#AT").val() < 2) ? mbti += "T" : mbti += "A";
+
+      window.location.href = mbti+".html";
+    }
+    else{
+        $("#title").html(q[num]["title"]); 
+        $("#type").val(q[num]["type"]); 
+        $("#A").html(q[num]["A"]); 
+        $("#B").html(q[num]["B"]);
+        num++; 
+    }
  }
  //답안 버튼 눌렀을 때
  $("#A").click(function () {
+    var type = $("#type").val();
+    var preValue = $("#" + type).val();
+    $("#" + type).val(parseInt(preValue) + 1);
     next();
     
 });
