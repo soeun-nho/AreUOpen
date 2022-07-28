f7efd679c692c65a5755a2c1d10dd6486d702d14
ehgmlKim <dohee930@gmail.com>
2022-07-28 12:41:12 +0900

    함수 간단하게 변경

--- Team_Project/script.js
+++ Team_Project/script.js
@@ -2,84 +2,34 @@ function start() {
     $("#start").hide();
     $("#q1").show();
 }
-function q1(){
-    $("#q1").hide();
-    $("#q2").show();
-}
-function q2(){
-    $("#q2").hide();
-    $("#q3").show();
-}
-function q3(){
-    $("#q3").hide();
-    $("#q4").show();
-}
-function q4(){
-    $("#q4").hide();
-    $("#q5").show();
-}
-function q5(){
-    $("#q5").hide();
-    $("#q6").show();
-}
-function q6(){
-    $("#q6").hide();
-    $("#q7").show();
-}
-function q7(){
-    $("#q7").hide();
-    $("#q8").show();
-}
-function q8(){
-    $("#q8").hide();
-    $("#q9").show();
-}
-function q9(){
-    $("#q9").hide();
-    $("#res").show();
+
+var num=1;
+var DU=0;
+var HS=0;
+var AT=0;
+
+function next(){
+    if(num == 9){
+        var result = "";
+        (DU < 2) ? result += "U" : result += "D";
+        (HS < 2) ? result += "S" : result += "H";
+        (AT < 2) ? result += "T" : result += "A";
+ 
+        window.location.href = "./result/" + result +".html";
+    }
+    else{
+        var next = num+1;
+        $("#q"+num).hide();
+        $("#q"+next).show();
+        num++;
+    }
 }
 
-var cnt=0;
 
-function cntChoice1(choice){
-    if(choice=='D')
-    cnt++;
-}
-function cntChoice2(choice){
-    if(choice=='A')
-    cnt++;
-}
-function cntChoice3(choice){
-    if(choice=='H')
-    cnt++;
+function count(choice){
+    if(choice=='D') { DU++; }
+    if(choice=='H') { HS++; }
+    if(choice=='A') { AT++; }
+    next();
 }
 
-function getChoice1(){
-    var x;
-
-    if(cnt>=2) // d == 0 
-        x=0;
-    else x=1;
-    return x*100;    
-}
-function getChoice3(){ 
-    var x;
-
-    if(cnt>=2) //h==0 
-        x=0;
-    else x=1;    
-    return x*10;
-}
-function getChoice2(){
-    var x;
-
-    if(cnt>=2) //a ==0
-        x=0;
-    else x=1;
-    return x;    
-}
-
-function answer(){
-    res = getChoice1()+getChoice2()+getChoice3();
-    
-}
