5a1a1b7994f1a6691d798c0bebe7de85af64dfa6
ehgmlKim <dohee930@gmail.com>
2022-08-02 17:27:41 +0900

    좌표 정확하게 수정, map의 level을 zoom으로 수정

--- AreUOpen/script.js
+++ AreUOpen/script.js
@@ -1,21 +1,21 @@
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
         mapOption = { 
-            center: new naver.maps.LatLng(37.606850, 127.042372), // 지도의 중심좌표
-            level: 3 // 지도의 확대 레벨
+            center: new naver.maps.LatLng(37.6034270, 127.0416957), // 지도의 중심좌표
+            zoom: 20 // 지도의 확대 레벨
         };
 
 var map = new naver.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
  
 // 마커를 표시할 위치와 title 객체 배열입니다 
 var positions = [
-    { title: '제나키친', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6036, 127.0417)},
-    { title:"조아버거", closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6042, 127.0408) },
-    { title: '송송식탁', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.604, 127.0427) },
-    { title: '스시빈', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6039, 127.0432) },
-    { title: '백소정', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6028, 127.0412)},
-    { title:"서브웨이", closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6039, 127.0427) },
-    { title: '핏짜피자', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6039, 127.042) },
-    { title: '샐러디', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6044,127.0428) }
+    { title: '제나키친', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6034, 127.04169) },
+    { title:"조아버거", closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6039015, 127.0408758) },
+    { title: '송송식탁', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6038977, 127.0427576) },
+    { title: '스시빈', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.60385, 127.0433) },
+    { title: '백소정', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6028850, 127.0412987)},
+    { title:"서브웨이", closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.60384, 127.04272) },
+    { title: '핏짜피자', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6037559, 127.0420138) },
+    { title: '샐러디', closeD: "F", openH:"11", openM:"0", colseH:"20", colseM:"0", latlng: new naver.maps.LatLng(37.6041401,127.0428911) }
 ];
 
  
